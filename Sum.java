179784b13ffd2b7f0c15b0f6bb08739d24932d51
kajalbhor05 <kajalbhor2311@gmail.com>
2025-08-20 09:00:54 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+public class Sum {
+public static void main(String[] args) {
+	int a=2;
+	int b=4;
+	System.out.println("Sum is:"+(a+b));
+	System.out.println("difference is:"+(a-b));
+	System.out.println("product is:"+(a*b));
+	System.out.println("division is:"+(a\b));
+}
+}
