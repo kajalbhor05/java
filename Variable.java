ae90f32aa010a8bed2cdb2412249b3294790640f
kajalbhor05 <kajalbhor2311@gmail.com>
2025-08-20 09:30:49 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,26 @@
+package Demo1;
+
+public class Variable {
+	static byte defaultByte;
+	static short defaultShort;
+	static int defaultInt;
+	static char defaultChar;
+	static float defaultFloat;
+	static long defaultLong;
+	static boolean defaultBoolean;
+	static double defaultDouble;
+	
+public static void main(String[] args) {
+	System.out.println("Default values of primitive datatypes:");
+	System.out.println("byte:"+defaultByte);
+	System.out.println("short:"+defaultShort);
+	System.out.println("int:"+defaultInt);
+	System.out.println("char:"+defaultChar);
+	System.out.println("float:"+defaultFloat);
+	System.out.println("long:"+defaultLong);
+	System.out.println("boolean:"+defaultBoolean);
+	System.out.println("double:"+defaultDouble);
+	
+	
+}
+}
