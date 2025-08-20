091132e35409cf021859309dacdc145fdccd323b
kajalbhor05 <kajalbhor2311@gmail.com>
2025-08-20 08:49:54 +0530

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,9 @@
+package Demo1;
+
+public class Info {
+public static void main(String[] args) {
+	System.out.println("name: Kajal");
+	System.out.println("age: 19");
+	System.out.println("city: Sangamner");
+}
+}
