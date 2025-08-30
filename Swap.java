d51e2ce5b036161c3a2d64a8aaa84715436fa6d2
kajalbhor05 <kajalbhor2311@gmail.com>
2025-08-30 18:55:42 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,15 @@
+package Demo1; 
+
+public class Swap{
+	int num=123;
+ public static void add() {
+    System.out.println(30/10);
+	System.out.println(20/10);
+	System.out.println(10/10);
+
+}
+
+public static void main(String[] args) {
+add();
+}
+}
