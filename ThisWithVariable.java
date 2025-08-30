0c10e712583b380af6d3860a221063e08e8afc37
kajalbhor05 <kajalbhor2311@gmail.com>
2025-08-30 18:57:30 +0530

    Add files via upload

---
+++ ThisWithVariable.java
@@ -0,0 +1,21 @@
+package Inheritance;
+class PQR{
+	int id;
+	String name;
+	 PQR(int id,String name)
+	 {
+	this.id=id;
+	this.name=name;
+	System.out.println("id:");
+	System.out.println("name:");
+		
+	}
+}
+public class ThisWithVariable {
+
+	public static void main(String[] args) {
+	PQR p1=new PQR(1,"Sham");
+
+	}
+
+}
