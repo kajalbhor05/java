5d8510016d1558da1baa5aa894402dbd87896637
kajalbhor05 <kajalbhor2311@gmail.com>
2025-08-30 18:55:14 +0530

    Add files via upload

---
+++ Swap1.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Swap1 {
+int num=123;
+public void Swap() {
+System.out.println("enter the number:"+num);
+}
+public void  add() {
+	System.out.println(30/10);
+	System.out.println(20/10);
+	System.out.println(10/10);
+}
+public static void main(String[]  args) {
+	Swap1 s1=new Swap1();
+	s1.add();
+}
+}
